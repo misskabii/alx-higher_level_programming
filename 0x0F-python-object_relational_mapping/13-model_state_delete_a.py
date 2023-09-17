514f8a066f1353c8cf514cc32ac889e782ebbfd6
misskabii <shazmakesh254@gmail.com>
2023-09-17 13:06:26 -0700

    0x0F

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-"""A script that changes the
-name of State object from the database hbtn_0e_6_us"""
-
-
-from sys import argv
+""" prints the State object with the name passed as argument from the database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-
-    import sys
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
-
-    session = Session(engine)
-    state = session.query(State).filter(State.id == 2).first()
-    state.name = 'New Mexico'
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).filter(State.name.like('%a%')):
+        session.delete(instance)
     session.commit()
-    session.close()
